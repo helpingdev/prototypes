b037cc708efda932dedf3ae1280590d291ce638b
gaspa03 <gaspa03@ca.com>
2019-02-27 18:09:46 +0100

    extract some value from context

--- devtest-dph-amd/src/main/java/com/ca/devtest/amd/dph/LoggerDPH.java
+++ devtest-dph-amd/src/main/java/com/ca/devtest/amd/dph/LoggerDPH.java
@@ -1,7 +1,15 @@
 package com.ca.devtest.amd.dph;
 
 
+import java.rmi.RemoteException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.lang.StringEscapeUtils;
+import org.eclipse.jetty.util.UrlEncoded;
+
 import com.itko.lisa.VSE;
+import com.itko.lisa.coordinator.VirtualService;
 import com.itko.lisa.test.TestExec;
 import com.itko.lisa.vse.stateful.model.Request;
 import com.itko.lisa.vse.stateful.model.Transaction;
@@ -11,7 +19,7 @@ import com.itko.util.GUID;
 
 public class LoggerDPH extends DataProtocol {
 
-	private static final String GUID_KEY="lisa.vse.transaction.guid";
+	private static final String INBOUND="lisa.vse.transaction.inbound";
 	private static final String MATCHED_TRANSACTION_PROPERTY="lisa.vse.matched.transaction";
 	
 
@@ -23,11 +31,17 @@ public class LoggerDPH extends DataProtocol {
 	
 	@Override
 	public void updateRequest(TestExec testExec, Request request) {
-		String guid=GUID.newGUID();	
-		String reqBody = request.getBodyAsString();
-		VSE.info(testExec, "Request uid: "+guid, request.toJSONString(true));
+			
+
 		// here you should parse the reqBody message and pull out the operation name and the data for the fields you are interested in
-		testExec.setStateValue(GUID_KEY, guid);
+		Map<String,String> data= new HashMap<String, String>();
+		data.put("Request_timestamp", String.valueOf(System.currentTimeMillis()));
+		data.put("Request_json", request.toString());
+		
+		
+		testExec.setStateValue(INBOUND, data);
+		
+		
 	}
 	
 
@@ -37,11 +51,31 @@ public class LoggerDPH extends DataProtocol {
 	@Override
 	public void updateResponse(TestExec testExec, TransientResponse transientResponse) {
 		
-		String guid= (String)testExec.getStateValue(GUID_KEY);
+		try {
+			
+		
 		Transaction transaction =(Transaction)testExec.getStateObject(MATCHED_TRANSACTION_PROPERTY);
-		Object  virtualService=testExec.getStateObject("LISA_VIRTUAL_SERVICE");
-		VSE.info(testExec, "Response uid: "+guid, transientResponse.getBodyAsString());
-		VSE.info(testExec, "Transaction uid: "+guid, transaction.toJSONString());
+		VirtualService  virtualService=(VirtualService)testExec.getStateObject("LISA_VIRTUAL_SERVICE");
+		
+		String requestJSON= transaction.getRequest().toJSONString(false);
+		
+		String transactionJson=transaction.toJSONString();
+		
+		String recordingName=virtualService.getName();
+		
+		String uuid=virtualService.getGroupTag();
+		String vseName=virtualService.getVirtualEnvironment().getName();
+		
+		
+				
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			
+		}finally {
+			
+		}
+	
 		
 		
 	}
